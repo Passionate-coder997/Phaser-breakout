ebf92d7fcc6583b444e39aac19e7b6f9d909611e
Jay <65998297+Passionate-coder997@users.noreply.github.com>
2021-04-07 19:22:58 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function create() {
         }
     }
 
-    paddle = game.add.sprite(game.world.centerX, 500, 'paddle');
+    paddle = game.add.sprite(game.world.centerX, 'paddle');
     paddle.anchor.setTo(0.5, 0.5);
 
     game.physics.enable(paddle, Phaser.Physics.ARCADE);
