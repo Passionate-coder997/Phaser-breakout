9c040510c7bceb8f397b00d30b73d2ed0df7e572
Jay <65998297+Passionate-coder997@users.noreply.github.com>
2021-04-08 16:53:02 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ function create() {
 
     for (var y = 0; y < 5; y++) {
         for (var x = 0; x < 15; x++) {
-            brick = bricks.create(2 + (x * 96), 85 + (y * 50), 'brick');
+            brick = bricks.create(2 + (x * 105), 85 + (y * 50), 'brick');
             brick.body.bounce.set(1);
             brick.body.immovable = true;
         }
